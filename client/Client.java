e99318e50d52d4087ce3b441147d0f9c93f7a78c
srihariraman04 <74083272+thealphacubicle@users.noreply.github.com>
2022-05-11 17:22:12 -0400

    Added server & client files

---
+++ client/Client.java
@@ -0,0 +1,77 @@
+import java.net.*;
+import java.io.*;
+
+public class Client
+{
+    //initialize socket and input/output streams
+    private Socket socket;
+    private DataInputStream din;
+    private DataOutputStream dout;
+
+    // constructor
+    public Client(String address, int port)
+    {
+        try
+        {
+            // create a socket to connect to the server
+            socket = new Socket(address, port);
+
+            // initialize input (recieve from terminal) and output streams through socket
+            BufferedReader d = new BufferedReader(new InputStreamReader(in));
+              dout = new DataOutputStream(socket.getOutputStream());
+        }
+
+        catch(UnknownHostException e)
+        {
+            System.out.println("Server not found: " + e.getMessage());
+        }
+
+        catch(IOException e)
+        {
+            System.out.println("I/O error: " + e.getMessage());
+        }
+
+        catch(Exception e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        String line = "";
+        
+        // while the user does not type "exit"
+        while(!line.equals("exit"))
+        {
+            try
+            {
+                // read a line from the terminal and then write it to the server
+                line = din.readLine();
+                dout.writeUTF(line);
+            }
+
+            catch(IOException e)
+            {
+                System.out.println("I/O error: " + e.getMessage());
+            }
+            finally{}
+        }
+
+        // close the socket
+        try
+        {
+            socket.close();
+            din.close();
+            dout.close();
+        }
+
+        catch(IOException e)
+        {
+            System.out.println("I/O error: " + e.getMessage());
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Client client = new Client("127.0.0.1", 5000);
+    }
+        
+}
---
+++ server/Server.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.net.*;
+
+public class Server
+{
+    // initialize socket and input/output streams
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private DataInputStream din;
+    private DataOutputStream dout;
+
+    //constructor
+    public Server(int port)
+    {
+        try
+        {
+            // create a socket to connect to the server
+            serverSocket = new ServerSocket(port);
+                System.out.println("Server is running...");
+
+            // wait for connection from client
+            socket = serverSocket.accept();
+
+            // initialize input (recieve from terminal) and output streams through socket
+            din = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            dout = new DataOutputStream(socket.getOutputStream());
+
+            String x = "";
+            //accept input while "over" is not entered
+            while(!din.readUTF().equals("over"))
+            {
+                x = din.readUTF();
+            }
+        }
+
+        catch(IOException ioe)
+        {
+            System.out.println("I/O Exception" +ioe);
+        }
+
+        catch(Exception e)
+        {
+            System.out.println("Exception" +e);
+        }
+
+        finally
+        {
+            // close connection
+            try
+            {
+                System.out.println("Closing connection...");
+                socket.close();
+                serverSocket.close();
+                din.close();
+                dout.close();
+            }
+
+            catch(IOException ioe)
+            {
+                System.out.println("I/O Exception" +ioe);
+            }     
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Server server = new Server(5000);
+    }
+}
